35194d6e17ee55e43ea87fc0a55b7941b079bc13
SimoBor99 <100366282+SimoBor99@users.noreply.github.com>
2023-06-23 16:55:36 +0200

    Update VisitSolver.cpp

--- airo2/visits_module/src/VisitSolver.cpp
+++ airo2/visits_module/src/VisitSolver.cpp
@@ -16,7 +16,7 @@
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
      */
 
-
+// Importing libraries that are needed
 #include "VisitSolver.h"
 #include "ExternalSolver.h"
 #include <map>
@@ -53,7 +53,7 @@ VisitSolver::~VisitSolver(){
 }
 
 void VisitSolver::loadSolver(string *parameters, int n){
-  starting_position = "r0";
+  starting_position = "r0";                 // Initialise our starting position as r0 (0,0)
   string Paramers = parameters[0];
 
   char const *x[]={"dummy"};
@@ -61,21 +61,18 @@ void VisitSolver::loadSolver(string *parameters, int n){
   parseParameters(Paramers);
   affected = list<string>(x,x+1);
   dependencies = list<string>(y,y+2);
-
-  std::cin>>k;
+  std::cout<<"Please insert the number of links between nodes ( the number should be between 5 and 30): "<<std::endl;
+  std::cin>>k;                            // The user input the desired K value
   std::cout<<std::endl;
-  while(k < 1 || k > 30) {
-    std::cout<<"Error! Please insert a value between 1 and 30"<<std::endl;
+  while(k < 5 || k > 30) {                // If the number is incorrect, the user is asked to insert a new value
+    std::cout<<"Error! Please insert a value between 5 and 30"<<std::endl;
     std::cin>>k;
     std::cout<<std::endl;
   }
 
-  string waypoint_file = "visits_domain/waypoint.txt";   // change this to the correct path
-  gen_rnd(waypoint_file);                                // Before parsing the waypoints, we generate the 24 random waypoints
+  string waypoint_file = "./waypoint.txt";
+  gen_rnd(waypoint_file);                             // Before parsing the waypoints, we generate the 24 random waypoints ont the file waypoint.txt
   parseWaypoint(waypoint_file);
-
-  string landmark_file = "visits_domain/landmark.txt";  // change this to the correct path
-  parseLandmark(landmark_file);
 
   build_graph();                                        // Now we build the graph to connect all the waypoints we've generated
 }
@@ -85,8 +82,6 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
   map<string, double>::iterator iSIt = initialState.begin();
   map<string, double>::iterator isEnd = initialState.end();
   double dummy;
-  double dummy2;
-  double act_cost;
 
   map<string, double> is_connected;
   map<string, double> trigger;
@@ -96,9 +91,6 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
     string parameter = iSIt->first;
     string function = iSIt->first;
     double value = iSIt->second;
-
-    //test(value,"test.txt");
-    //test3(function,"test2.txt");
 
     function.erase(0,1);
     function.erase(function.length()-1,function.length());
@@ -115,33 +107,24 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
         if (value>0){
       string from = tmp.substr(0,2);   // from and to are regions, need to extract wps (poses)
       string to = tmp.substr(3,2);
-      // HERE ARE EXTRACTED THE NAME OF THE REGIONS (SUCH AS r0, r1 AND SO ON...)
-
-
-      act_dist=compute_path(from, to);
+                                              // Here we compute the cost for the movement between two regions;
+      act_dist=compute_path(from, to);        // the cost is equal to the total path covered by the robot, passing through the waypoints
       if(act_dist == 10000) {
-        act_dist = -1000;
+        act_dist = -1000;                     // If the path does not exist, the cost is assigned to a negative value (with K >= 5, this should not happen!)
       }
-      //test(act_dist,"partial_dist.txt");
 
     }
   }
 }else{
   if(function=="dummy"){
-    dummy = act_dist;
+    dummy = act_dist;                         // The cost for the movement between two regions is assigned to dummy
 
   }
-  else if(function=="act-cost"){
-    act_cost = value;
-                 } //else if(function=="dummy1"){
-                    //duy = value;              
-                    ////cout << parameter << " " << value << endl;
-                 //}egions[reg[i]] 
                  }
                }
 
 
-               double results = calculateExtern(dummy, act_cost);
+               double results = calculateExtern();
                if (ExternalSolver::verbose){
                 cout << "(dummy) " << results << endl;
               }
@@ -187,7 +170,7 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
 
          }
 
-         double VisitSolver::calculateExtern(double external, double total_cost){
+         double VisitSolver::calculateExtern(){
        double cost = act_dist;
        return cost;
      }
@@ -214,52 +197,23 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
 
          pose3 = (double)atof(line.substr(curr,next-curr).c_str());
 
-         waypoint[waypoint_name] = vector<double> {pose1, pose2, pose3};  //HERE ARE EXTRACTED THE WAYPOINTS POSITION
+         waypoint[waypoint_name] = vector<double> {pose1, pose2, pose3};  // Here are the extracted waypoints positions
        }
      }
 
    }
-   // NOTA: IL CICLO WHILE (^) VIENE ESEGUITO 30 VOLTE (QUINDI TUTTO OK!)
 
-   void VisitSolver::parseLandmark(string landmark_file){
-
-     int curr, next;
-     string line;
-     double pose1, pose2, pose3;
-     ifstream parametersFile(landmark_file);
-     if (parametersFile.is_open()){
-      while (getline(parametersFile,line)){
-       curr=line.find("[");
-       string landmark_name = line.substr(0,curr).c_str();
-       
-       curr=curr+1;
-       next=line.find(",",curr);
-
-       pose1 = (double)atof(line.substr(curr,next-curr).c_str());
-       curr=next+1; next=line.find(",",curr);
-
-       pose2 = (double)atof(line.substr(curr,next-curr).c_str());
-       curr=next+1; next=line.find("]",curr);
-
-       pose3 = (double)atof(line.substr(curr,next-curr).c_str());
-
-       landmark[landmark_name] = vector<double> {pose1, pose2, pose3};  //SAME AS PARSEWAYPOINT
-     }
-   }
-   
- }
-
+  // Function to compute the cost (distance) between two regions, given all the waypoints
   double VisitSolver::compute_path(string from, string to){
-    //print_matrix("matrix.txt");
-    int f = extract_num(from);
+    int f = extract_num(from);                              // Initialize stuff...
     int t = extract_num(to);
     double distances[30] = {};
-    for (int o = 0; o < 30; o++) {
-      distances[o] = 10000;
+    for (int o = 0; o < 30; o++) {                            // Initialize distances to infinity (a high value) 
+      distances[o] = 10000;                                 
     }
     bool visited[30] = {};
     distances[f] = 0;
-    int n_visited = 1;
+    int n_visited = 1;                                        // Using the Dijkstra algorithm, we compute the total cost of the robot's movement
     while(n_visited < 30) {
       int u = find_unvis(distances, visited);
       n_visited++;
@@ -270,10 +224,10 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
         }
       }
     }
-    test(distances[t],"partial_distance.txt");
     return distances[t];
   }
 
+  // This function is used in the Dijkstra algorithm to find the unvisited node with the minimum distance from the source
   int VisitSolver::find_unvis(double distances[30], bool visited[30]) {
     int min_dist = INT_MAX;
     int min_idx = -1;
@@ -286,22 +240,24 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
     return min_idx;
   }
 
+  // This function i used to compute the euclidean distance between two waypoints
   double VisitSolver::distance_euc(string from, string to){
     map <string, string> regions;
     for(int i = 0; i < 30; i++) {
       regions[reg[i]] = wp[i];
     }
 
-    double x1 = waypoint[regions[from]].at(0);
+    double x1 = waypoint[regions[from]].at(0);      // We extract the coordinates of the two waypoints
     double y1 = waypoint[regions[from]].at(1);
     
     double x2 = waypoint[regions[to]].at(0);
     double y2 = waypoint[regions[to]].at(1);
     
-    distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2));
+    distance = sqrt(pow((x2-x1),2) + pow((y2-y1),2));     // The euclidean distance is computed
     return distance;
   }
 
+  // This function build a graph connecting the waypoints 
   void VisitSolver::build_graph() {
     int flag = 0;
     int min_idx;
@@ -314,52 +270,36 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
           string b = to_string(j);
           a = "r" + a;
           b = "r" + b;
-          dist_matrix[i][j] = distance_euc(a,b);
-          useful_array[j] = dist_matrix[i][j];
-          //test(dist_matrix[i][j],"test_val.txt");
+          dist_matrix[i][j] = distance_euc(a,b);  // We compute the distance between every couple of waypoints and put the result in dist_matrix
+          useful_array[j] = dist_matrix[i][j];             // Useful array is used to find the minimum distances
         }
         else {
           dist_matrix[i][j] = 1000.0;          // Elements on the diagonal are set to a high value, so they do not interfere in the search for the minimum
           useful_array[j] = dist_matrix[i][j];
-          //test(dist_matrix[i][j],"test.txt");     // UNCOMMENT FOR DEBUGGING
+
         }
       }
-      while(n_links[i] < k && flag < 30) {
-        min_idx = find_min(); 
-        if (n_links[min_idx] < k) {
+      while(n_links[i] < k && flag < 30) {    // While the number of connection for the current waypoint are less than K,
+        min_idx = find_min();                 // we keep connecting it to the closest waypoints
+        if (n_links[min_idx] < k) {           // If the closest waypoint is not already connected to K waypoints, we connect it to the current one
           adj_matrix[i][min_idx] = 1;
           adj_matrix[min_idx][i] = 1;
           n_links[i]++;
           n_links[min_idx]++;
         }
         else {
-          flag++;
+          flag++;                             // This variable is used to avoid infinite loops
         }
       }
-    }
-    for(int i = 0; i < 30; i++) {
-      for(int j = 0; j < 30; j++) {
-        if(adj_matrix[i][j] == 0) {
-          test(0.0,"adj.txt");              // UNCOMMENT FOR DEBUGGING
-        }
-        else if (adj_matrix[i][j] == 1) {
-          test(1.0,"adj.txt");              // UNCOMMENT FOR DEBUGGING
-          test2(i,"conn.txt");
-          test2(j,"conn.txt");
-          test3(" ","conn.txt");
-        }
-      }
-      test3(" ","adj.txt");
     }
   }
 
+  // This function is used to find the minimum element in useful_array
   int VisitSolver::find_min() {
     int min = useful_array[0];
     int min_index = 0;
-    //test(useful_array[0],"adj.txt");
     for(int l = 1; l < 30; l++) {
       if(useful_array[l] < min) {
-        //test(useful_array[l],"adj.txt");
         min_index = l;
         min = useful_array[l];
       }
@@ -368,86 +308,27 @@ map<string,double> VisitSolver::callExternalSolver(map<string,double> initialSta
     return min_index;
   }
 
-  void VisitSolver::test(double d, const std::string& nomeFile) {
-    std::ofstream file(nomeFile, std::ios::app);
-    if (file.is_open()) {
-        file << d;
-        file << " ";
-        file.close();
-        //std::cout << "Numero reale scritto sul file con successo." << std::endl;
-    } else {
-        std::cout << "Impossibile aprire il file." << std::endl;
-    }
-  }
-
-  void VisitSolver::test2(int i, const std::string& nomeFile) {
-    std::ofstream file(nomeFile, std::ios::app);
-    if (file.is_open()) {
-        file << i;
-        file << " ";
-        file.close();
-        //std::cout << "Numero reale scritto sul file con successo." << std::endl;
-    } else {
-        std::cout << "Impossibile aprire il file." << std::endl;
-    }
-  }
-
-  void VisitSolver::test3(string s, const std::string& nomeFile) {
-    std::ofstream file(nomeFile, std::ios::app);
-    if (file.is_open()) {
-        file << s;
-        file << "\n";
-        file.close();
-        //std::cout << "Numero reale scritto sul file con successo." << std::endl;
-    } else {
-        std::cout << "Impossibile aprire il file." << std::endl;
-    }
-  }
-
-  void VisitSolver::print_matrix(const std::string& nomeFile) {
-    std::ofstream file(nomeFile, std::ios::app);
-    if (file.is_open()) {
-        for (int i = 0; i < 30; i++) {
-          for (int j = 0; j < 30; j++) {
-            file << adj_matrix[i][j];
-            file << " ";
-          }
-          file << "\n";
-        }
-        file.close();
-        //std::cout << "Numero reale scritto sul file con successo." << std::endl;
-    } else {
-        std::cout << "Impossibile aprire il file." << std::endl;
-    }
-  }
-
   void VisitSolver::gen_rnd(string p) {
-    // Initialise stuff...
-    float waypoints[24][3];
-    // Open the file waypoint.txt, to generate on it the random waypoints
-    ofstream outfile;
-    // We first open the file in trunc mode and then close it, so the waypoints.txt file will be emptied
+    float waypoints[24][3];               // Initialise stuff...
+    ofstream outfile;                     // Open the file waypoint.txt, to generate on it the random waypoints
     outfile.open(p, ios_base::trunc);
-    outfile.close();
+    outfile.close();                      // We first open the file in trunc mode and then close it, so the waypoints.txt file will be emptied
     outfile.open(p, ios_base::app);
-    // Write the five waypoints that are already known
-    outfile<<"wp0[0,0,0]"<<std::endl;
+    outfile<<"wp0[0,0,0]"<<std::endl;     // Write the six waypoints that are already known
     outfile<<"wp1[-2.5,2.5,0]"<<std::endl;
     outfile<<"wp2[2.5,2.5,1.57]"<<std::endl;
     outfile<<"wp3[-2.5,-2.5,3.14]"<<std::endl;
     outfile << "wp4[2.5,-2.5,-1.57]" << std::endl;
     outfile << "wp5[3,0,0]" << std::endl;
-    // Initialize the random number generator 
-    std::random_device rd;
+    std::random_device rd;                // Initialize the random number generator 
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(-3.0, 3.0);
-    // Setting random value for x,y waypoints
-    for (int i = 6; i <= 29; i++){
+    for (int i = 6; i <= 29; i++){        // Setting random value for x,y waypoints
         for(int j = 0; j <= 2; j++){
             waypoints[i][j] = dis(gen);
-            waypoints[i][j] = std::trunc(waypoints[i][j] * 100.0) / 100.0;
+            waypoints[i][j] = std::trunc(waypoints[i][j] * 100.0) / 100.0;    // We truncate the real number to the second decimal digit
             if(j==0) {
-              outfile<<"wp"<<i<<"["<<waypoints[i][j]<<",";
+              outfile<<"wp"<<i<<"["<<waypoints[i][j]<<",";                      // We write the waypoints on the file 
             }else if(j==1) {
               outfile<<waypoints[i][j]<<",";
             }
