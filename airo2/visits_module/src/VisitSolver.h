d10c488710bf821c203f99d971924cdf79f68627
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2023-06-23 15:06:57 +0200

    Update VisitSolver.h

--- airo2/visits_module/src/VisitSolver.h
+++ airo2/visits_module/src/VisitSolver.h
@@ -1,22 +1,3 @@
-/*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2015  <copyright holder> <email>
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
 #ifndef TESTSOLVER_H
 #define TESTSOLVER_H
 
@@ -42,6 +23,13 @@ public:
     map<string, vector<double>> waypoint;
     map<string, vector<double>> landmark;
     double distance;
+    double act_dist;
+    double dist_matrix[30][30];
+    double useful_array[30];
+    int adj_matrix[30][30] = {};
+    int k;
+    string wp[30] = {"wp0","wp1","wp2","wp3","wp4","wp5","wp6","wp7","wp8","wp9","wp10","wp11","wp12","wp13","wp14","wp15","wp16","wp17","wp18","wp19","wp20","wp21","wp22","wp23","wp24","wp25","wp26","wp27","wp28", "wp29"};
+    string reg[30] = {"r0","r1","r2","r3","r4","r5","r6","r7","r8","r9","r10","r11","r12","r13","r14","r15","r16","r17","r18","r19","r20","r21","r22","r23","r24","r25","r26","r27","r28", "r29"};
    
     void parseWaypoint(string waypoint_file);
     void parseLandmark(string landmark_file);
@@ -50,12 +38,19 @@ public:
     vector <string> source, target; 
     string starting_position;
      
-   
-   
+    int find_min();
+    void build_graph();
     void parseParameters(string parameters);
-    void distance_euc(string from, string to);
+    double distance_euc(string from, string to);
+    double compute_path(string from, string to);
+    int find_unvis(double distances[30], bool visited[30]);
     void test(double d, const std::string& nomeFile);
+    void test2(int i, const std::string& nomeFile);
+    void test3(string s, const std::string& nomeFile);
+    void print_matrix(const std::string& nomeFile);
+
     void gen_rnd(string p);
+    int extract_num(string r);
 
 private:
     list<string> affected;
